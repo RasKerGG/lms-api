8d2a986a9368094e5263c2b301a92ef88a0a21bd
Vladimir <mezenkovvladimir06@gmail.com>
2026-08-28 10:18:29 +0300

    feat: первоначальная настройка swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder().setTitle('LMS API').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 }
 bootstrap();
