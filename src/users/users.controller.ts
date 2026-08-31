955d971e48acf3303be4137a7a87f22a46249cf6
Vladimir <mezenkovvladimir06@gmail.com>
2026-08-31 11:04:32 +0300

    feat: базовые CRUDы для пользователя

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() dto: CreateUserDto) {
+    return this.usersService.createUser(dto);
+  }
+
+  @Get(':uuid')
+  async getUser(@Param('uuid', ParseUUIDPipe) uuid: string) {
+    return this.usersService.findUser(uuid);
+  }
+
+  @Delete(':uuid')
+  async deleteUser(@Param('uuid', ParseUUIDPipe) uuid: string) {
+    return this.usersService.deleteUser(uuid);
+  }
+
+  @Get()
+  async getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/CreateUser.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(dto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: dto,
+    });
+  }
+
+  async findUser(id: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async deleteUser(id: string) {
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async getAllUsers() {
+    return this.prisma.user.findMany();
+  }
+}
